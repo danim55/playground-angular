1e43ac9da035bc3cda27d978e568074f3e9cc28f
danim55 <dmerinocano@gmail.com>
2023-01-30 22:38:55 +0100

    Angular: services section; adding a data service

---
+++ services-start/src/app/accounts.service.ts
@@ -0,0 +1,23 @@
+export class AccountsService {
+    accounts = [
+        {
+            name: 'Master Account',
+            status: 'active'
+        },
+        {
+            name: 'Testaccount',
+            status: 'inactive'
+        },
+        {
+            name: 'Hidden Account',
+            status: 'unknown'
+        }
+    ];
+
+    addAcount(name: string, status: string) {
+        this.accounts.push({ name: name, status: status });
+    }
+    updateStatus(id: number, newStatus: string) {
+        this.accounts[id].status = newStatus;
+    }
+}
