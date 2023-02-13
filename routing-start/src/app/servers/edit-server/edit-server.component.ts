dd8aab33592a9e892303316b34643cb5ba87b9a7
danim55 <dmerinocano@gmail.com>
2023-02-13 16:42:26 +0100

    Interface and service to dialog before leaving page without saving

--- routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 import { ServersService } from '../servers.service';
+import { CanComponentDeactivate } from './can-deactivate-guard.service';
 
 @Component({
   selector: 'app-edit-server',
   templateUrl: './edit-server.component.html',
   styleUrls: ['./edit-server.component.css']
 })
-export class EditServerComponent implements OnInit {
+export class EditServerComponent implements OnInit, CanComponentDeactivate {
   server: { id: number, name: string, status: string };
   serverName = '';
   serverStatus = '';
   allowEdit = false;
+  changesSaved = false;
 
-  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
+  constructor(private serversService: ServersService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
     this.route.queryParams
@@ -28,7 +33,7 @@ export class EditServerComponent implements OnInit {
     this.route.params
       .subscribe(
         (params: Params) => {
-            this.server = this.serversService.getServer(+params['id']);
+          this.server = this.serversService.getServer(+params['id']);
         }
       )
     this.serverName = this.server.name;
@@ -37,6 +42,20 @@ export class EditServerComponent implements OnInit {
 
   onUpdateServer() {
     this.serversService.updateServer(this.server.id, { name: this.serverName, status: this.serverStatus });
+    this.changesSaved = true;
+    this.router.navigate(['../'], { relativeTo : this.route })
+  }
+
+  canDeactivate(): boolean | Observable<boolean> | Promise<boolean> {
+    if(!this.allowEdit){
+      return true;
+    }
+    if((this.serverName != this.server.name || this.serverStatus != this.server.status) &&
+    !this.changesSaved){
+      return confirm('Do you want to discard the changes?')
+    }else{
+      return true;
+    }
   }
 
 }
