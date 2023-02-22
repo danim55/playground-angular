876638215c595c85259683c7c751575db3c060ad
danim55 <dmerinocano@gmail.com>
2023-02-22 12:50:38 +0100

    Adding property binding to have default value

--- td-forms-start/src/app/app.component.ts
+++ td-forms-start/src/app/app.component.ts
@@ -9,6 +9,7 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
 
   @ViewChild('form') signUpForm: NgForm;
+  defaultSelectValue = "teacher";
 
   suggestUserName() {
     const suggestedName = 'Superuser';
