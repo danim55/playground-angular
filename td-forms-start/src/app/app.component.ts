4fcfb3a83a8f392f9bb3265600e7e02de7ff0291
danim55 <dmerinocano@gmail.com>
2023-02-23 13:47:23 +0100

    NgModle with two-way binding

--- td-forms-start/src/app/app.component.ts
+++ td-forms-start/src/app/app.component.ts
@@ -9,7 +9,9 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
 
   @ViewChild('form') signUpForm: NgForm;
+  
   defaultSelectValue = "teacher";
+  answer : string = '';
 
   suggestUserName() {
     const suggestedName = 'Superuser';
