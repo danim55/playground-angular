2b4b6f9e8d38b4b99105f12307b6996e92ea51d0
danim55 <dmerinocano@gmail.com>
2023-02-27 22:01:01 +0100

    Adding validator and submiting form

--- reactive-forms-start/src/app/app.component.ts
+++ reactive-forms-start/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -12,10 +12,13 @@ export class AppComponent implements OnInit{
 
   ngOnInit(){
     this.signUpForm = new FormGroup({
-      'username': new FormControl(null),
-      'mail': new FormControl(null),
+      'username': new FormControl(null, Validators.required),
+      'mail': new FormControl(null, [Validators.required, Validators.email]),
       'gender': new FormControl('male'),
     })
   }
 
+  onSubmit(){
+    console.log(this.signUpForm);
+  }
 }
