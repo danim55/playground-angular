70a6ba20ee903b5e8225879ecddadf15617a676c
danim55 <dmerinocano@gmail.com>
2023-03-29 22:54:03 +0200

    Using rxjs operators to transform response data

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -11,15 +12,16 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) { }
 
-  ngOnInit() { 
+  ngOnInit() {
     this.fetchPosts()
   }
 
   onCreatePost(postData: { title: string; content: string }) {
-    this.http.post('https://http-angular-course-25d77-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
-      postData).subscribe(responseData => {
-        console.log(responseData);
-      })
+    this.http
+      .post('https://http-angular-course-25d77-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
+        postData).subscribe(responseData => {
+          console.log(responseData);
+        })
   }
 
   onFetchPosts() {
@@ -30,8 +32,18 @@ export class AppComponent implements OnInit {
     // Send Http request
   }
 
-  fetchPosts() {
-    this.http.get('https://http-angular-course-25d77-default-rtdb.europe-west1.firebasedatabase.app/posts.json')
+  private fetchPosts() {
+    this.http
+      .get('https://http-angular-course-25d77-default-rtdb.europe-west1.firebasedatabase.app/posts.json')
+      .pipe(map(responseData => {
+        const postArray = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postArray.push({ ...responseData[key], id: key })
+          }
+        }
+        return postArray;
+      }))
       .subscribe(posts => {
         console.log(posts);
       })
