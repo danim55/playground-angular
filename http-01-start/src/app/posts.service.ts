ba141af7757fea49b7ff3b2fcf12d68c80ecaf26
danim55 <dmerinocano@gmail.com>
2023-03-31 22:17:55 +0200

    Adding clear posts method (delete call)

--- http-01-start/src/app/posts.service.ts
+++ http-01-start/src/app/posts.service.ts
@@ -6,28 +6,36 @@ import { Post } from "./post.module";
 @Injectable({ providedIn: 'root' })
 export class PostsService {
 
-    constructor(private http: HttpClient){}
+  constructor(private http: HttpClient) { }
 
-    createAndStorePost(title: string, content: string){
-        const postData: Post = {title: title, content: content  }
-        this.http
-        .post<{ name: string }>('https://http-angular-course-25d77-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
-          postData).subscribe(responseData => {
-            console.log(responseData);
-          })
-    }
+  createAndStorePost(title: string, content: string) {
+    const postData: Post = { title: title, content: content }
+    this.http
+      .post<{ name: string }>('https://http-angular-course-25d77-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
+        postData).subscribe(responseData => {
+          console.log(responseData);
+        })
+  }
 
-    fetchPosts(){
-        return this.http
-        .get<{ [key: string]: Post }>('https://http-angular-course-25d77-default-rtdb.europe-west1.firebasedatabase.app/posts.json')
-        .pipe(map(responseData => {
-          const postArray: Post[] = [];
-          for (const key in responseData) {
-            if (responseData.hasOwnProperty(key)) {
-              postArray.push({ ...responseData[key], id: key })
-            }
+  fetchPosts() {
+    return this.http
+      .get<{ [key: string]: Post }>('https://http-angular-course-25d77-default-rtdb.europe-west1.firebasedatabase.app/posts.json')
+      .pipe(map(responseData => {
+        const postArray: Post[] = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postArray.push({ ...responseData[key], id: key })
           }
-          return postArray;
-        }));
-    }
+        }
+        return postArray;
+      }));
+  }
+
+  deletePosts() {
+    this.http
+      .delete('https://http-angular-course-25d77-default-rtdb.europe-west1.firebasedatabase.app/posts.json')
+      .subscribe(respose => {
+        console.log(respose);
+      })
+  }
 }
