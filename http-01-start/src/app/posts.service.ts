708d024ede82fca24303a3d5a22478e9804244db
danim55 <dmerinocano@gmail.com>
2023-04-02 19:09:08 +0200

    Checking all the posibilities of the responses

--- http-01-start/src/app/posts.service.ts
+++ http-01-start/src/app/posts.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { HttpClient, HttpEventType, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Subject, throwError } from "rxjs";
-import { catchError, map } from "rxjs/operators";
+import { catchError, map, tap } from "rxjs/operators";
 import { Post } from "./post.module";
 
 @Injectable({ providedIn: 'root' })
@@ -14,18 +14,23 @@ export class PostsService {
   createAndStorePost(title: string, content: string) {
     const postData: Post = { title: title, content: content }
     this.http
-      .post<{ name: string }>('https://http-angular-course-25d77-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
-        postData).subscribe(responseData => {
-          console.log(responseData);
-        }, error => {
-          this.error.next(error);
+      .post<{ name: string }>(
+        'https://http-angular-course-25d77-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
+        postData,
+        {
+          observe: 'response',
         })
+      .subscribe(responseData => {
+        console.log(responseData);
+      }, error => {
+        this.error.next(error);
+      })
   }
 
   fetchPosts() {
     let customParams = new HttpParams();
     customParams = customParams.append('print', 'pretty');
-    customParams = customParams.append('custom','ronaldo');
+    customParams = customParams.append('custom', 'ronaldo');
 
     return this.http
       .get<{ [key: string]: Post }>(
@@ -52,6 +57,19 @@ export class PostsService {
 
   deletePosts() {
     return this.http
-      .delete('https://http-angular-course-25d77-default-rtdb.europe-west1.firebasedatabase.app/posts.json');
+      .delete(
+        'https://http-angular-course-25d77-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
+        {
+          observe: 'events',
+        }
+      ).pipe(tap(event => {
+        if (event.type === HttpEventType.Sent) {
+          console.log('Data send');
+        }
+        else if (event.type === HttpEventType.Response) {
+          console.log('Info received')
+          console.log(event.body);
+        }
+      }));
   }
 }
